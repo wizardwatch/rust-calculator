b64f2456c3cf77d16daecf3894e818979ede8563
wyatt osterling <wyatt.osterling@hotmail.com>
2020-09-14 11:42:23 -0400

    solve string is a cargo container and is less dumb

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,8 @@ use kiss_ui::button::*;
 use kiss3d::light::Light;
 use kiss3d::window::Window;
 use na::Point3;
-use kiss3d::event::Key::*;
-use kiss3d::event::MouseButton::*;
-use kiss3d::event::*;
-
+mod solvestring;
+use crate::solvestring::solvestring::solve_string;
 fn main(){
         kiss_ui::show_gui(|| {
             Dialog::new(
@@ -55,12 +53,12 @@ fn main(){
         Ok(_) => {
             println!("input received")
         },
-        Err(e) => println!("input faiwed owO. Pwease repowt to github uwu.")
+        Err(_e) => println!("input faiwed owO. Pwease repowt to github uwu.")
     }
 }
 
 fn replace(current: f32, equRaw: String) -> String {
-    let mut equNew:String = "owo".to_string();
+    let equNew:String;
     let equRaw2 = &equRaw;
     let replacewith:String = current.to_string();
     if contains(equRaw.to_string(), 'x'.to_string())!=-1{
@@ -90,9 +88,9 @@ fn repeater(equation: String, min: String, max: String, rate: String, x: &mut Ve
     let fmax:f32 = max.parse().unwrap();
     let frate:f32 = rate.parse().unwrap();
     let equRaw = equation;
-    let mut equNew:String = "owo".to_string();
-    let mut result:f32= 0.0;
-    while !(i > (fmax)) {
+    let mut equNew:String;
+    let mut result:f32;
+    while i <= fmax {
         equNew = replace(i, equRaw.to_string());
         x.push(i);
         println!("1: Solving {}",equNew);
@@ -144,8 +142,6 @@ fn passthrough(equation: String, min: String, max: String, rate: String, mut x1:
     println!("7: Finished Graph")
 }
 
-
-
 fn graph(x1:&Vec<f32>, y1:&Vec<f32>, z1:&Vec<f32>){
     let mut corda;
     let mut cordb;
@@ -186,226 +182,5 @@ fn graph(x1:&Vec<f32>, y1:&Vec<f32>, z1:&Vec<f32>){
             cordb = Point3::new(bx, by, bz);
             window.draw_line(&corda, &cordb, &Point3::new(1.0, 0.0, 0.0));
         }
-    }
-}
-
-fn solve_string(mut input:String) -> f32 {
-    println!("The original input is {}", input);
-    //declares stuff.
-    let mut answer:f32 = 404.0;
-    let mut operator_places = vec![];
-    let exponent = "^";
-    let multiplication = "*";
-    let division = "/";
-    let addition = "+";
-    let subtraction = "~";
-    let mut current_letter;
-    let mut num_of_parth = 0;
-    let mut most_inner_parth = 0;
-    let mut places_after_most_inner = 0;
-    let mut runner = 0;
-    //creates a clone of input in order to retain the original input.
-    let before_input = input.clone();
-    //adds parenthesis on either side of the equation.
-    input = "".to_string();
-    input.push_str("(");
-    input.push_str(&before_input);
-    input.push_str(")");
-    //finds the number of opening parenthesis
-    for i in 0..input.len() {
-        current_letter = &input[i..i + 1];
-        if current_letter == "("{
-            num_of_parth = num_of_parth +1
-        }
-    }
-    //runs the solving part of solve string once for each opening parenthesis
-    while num_of_parth > 0 {
-        //resets variables
-        most_inner_parth = 0;
-        places_after_most_inner = 0;
-        runner = 0;
-        println!("I repeated num_of_parth");
-        //determines the location of the innermost opening and closing parenthesis
-        for i in 0..input.len() {
-            current_letter = &input[i..i + 1];
-            if current_letter == "("{
-                most_inner_parth = most_inner_parth + runner;
-                runner = 0;
-            }
-            else if current_letter == ")"{
-                break;
-            }
-            runner = runner + 1;
-        }
-        println!{"most inner_parth = {}", most_inner_parth};
-        for i in most_inner_parth..input.len() {
-            current_letter = &input[i..i+1];
-            println!("ran most inner parth = {}", most_inner_parth);
-            if current_letter == ")"{
-                break;
-            }
-            else{
-                places_after_most_inner = places_after_most_inner+1;
-                println!("iterated places after most inner");
-            }
-        }
-        // creates an input to be fed to the operator detector and solver.
-        let mut new_input = (&input[(most_inner_parth+1) as usize..(most_inner_parth +places_after_most_inner) as usize]).to_string();
-        //adds operations in the order of pemdas
-        for i in 0..new_input.len() {
-            current_letter = &new_input[i..i + 1];
-            if current_letter == exponent {
-                operator_places.push(exponent);
-            }
-        }
-        println!(" This  is the input as of line 247 {}", new_input);
-        for i in 0..new_input.len() {
-            current_letter = &new_input[i..i + 1];
-            if current_letter == multiplication {
-                operator_places.push(multiplication);
-            } else if current_letter == division {
-                operator_places.push(division);
-            }
-        }
-        for i in 0..new_input.len() {
-            current_letter = &new_input[i..i + 1];
-            if current_letter == addition{
-                operator_places.push(addition);
-            }
-
-            else if current_letter == subtraction{
-                operator_places.push(subtraction);
-            }
-        }
-        //finds the place of the current operator.
-        for i in 0..operator_places.len() {
-            let mut beforeplaces = 1;
-            let mut afterplaces = 1;
-            let current_operator = operator_places[i];
-            let mut int_cop:i32 = 0;
-            for s in 0..new_input.len(){
-                println!("test 1");
-                current_letter = &new_input[s..s + 1];
-                if current_operator == current_letter{
-                    println!("test 2");
-                    let str_cop = s.to_string();
-                    int_cop = str_cop.parse::<i32>().unwrap();
-                }
-            }
-            let mut ch_p:usize;
-            //determines the number of places before the operator that are numbers
-            loop{
-                println!(" This  is the input as of line 256 {}", new_input);
-                println!("int_cop = {}", int_cop);
-                ch_p = (int_cop - beforeplaces) as usize;
-                println!("ch_p = {}", ch_p);
-                if ch_p == 0{
-                    break;
-                }
-                let ch = &new_input[ch_p-1..ch_p];
-                let chstring = ch.to_string();
-                if true ==  (is_string_numeric(chstring)){
-                    beforeplaces = beforeplaces + 1;
-                }
-                else if true ==  (ch.to_string() == "."){
-                    beforeplaces = beforeplaces + 1;
-                }
-                else if true ==  (ch.to_string() == "-"){
-                    beforeplaces = beforeplaces + 1;
-                }
-                else{
-                    break;
-                }
-            }
-            //determines the number of places after the operator that are numbers.
-            loop{
-                ch_p = (int_cop  + afterplaces + 1) as usize;
-                if ch_p == new_input.len(){
-                    break;
-                }
-                let ch = &new_input[ch_p..ch_p+1];
-                let chstring = ch.to_string();
-                if true == (is_string_numeric(chstring)){
-                    afterplaces = afterplaces + 1;
-                }
-                else if true == (ch.to_string() == "."){
-                    afterplaces = afterplaces + 1;
-                }
-                else if true == (ch.to_string() == "-"){
-                    afterplaces = afterplaces + 1;
-                }
-                else{
-                    break;
-                }
-                println!("Afterplaces iterated");
-            }
-            println!("{}", beforeplaces);
-            //takes new input and before places and comes up with first number to be operatored on.
-            let firstnum = &new_input[(int_cop-beforeplaces) as usize..(int_cop) as usize];
-            let firstnumf32:f32 = firstnum.parse::<f32>().unwrap();
-            println!("2: firstnum is {}",firstnum);
-            //does the same as first num except after places.
-            let mut secnum;
-            secnum =&new_input[(int_cop+1) as usize..(int_cop+afterplaces+1) as usize];
-            println!("3: secnum is {}",secnum);
-            let secnumf32:f32 = secnum.parse::<f32>().unwrap();
-            //does the corresponding math as requested by current operator.
-            if current_operator == exponent{
-                answer = firstnumf32.powf(secnumf32);
-            }
-            if current_operator ==  multiplication{
-                answer = firstnumf32 * secnumf32;
-            }
-            if current_operator == division{
-                answer = firstnumf32 / secnumf32;
-            }
-            if current_operator == addition{
-                answer = firstnumf32 + secnumf32;
-            }
-            if current_operator == subtraction{
-                answer = firstnumf32 - secnumf32;
-            }
-            //edits new input to equal new input minus the operator and numbers acted upon.
-            println!("The answer is {}", answer);
-            let mut coolstring = new_input;
-            println!("4: beforeplaces is {}\n5: afterplaces is {}",beforeplaces,afterplaces);
-            let inputcash1 = &coolstring[0 as usize..int_cop as usize-beforeplaces as usize];
-            let inputcash2 = answer.to_string();
-            let inputcash3 = &coolstring[(int_cop +afterplaces+1) as usize .. coolstring.len() as usize];
-            new_input = ("").parse().unwrap();
-            new_input.push_str(inputcash1);
-            new_input.push_str(&inputcash2);
-            new_input.push_str(inputcash3);
-
-        }
-        let mut new_input = (&input[(most_inner_parth+1) as usize..(most_inner_parth +places_after_most_inner) as usize]).to_string();
-        //lets input be modified from the answer of the parenthesis
-        println!("The answer is {}", answer);
-        let mut inputclone = input.clone();
-        let inputcash1 = &inputclone[0 as usize..most_inner_parth as usize];
-        let inputcash2 = answer.to_string();
-        let inputcash3 = &inputclone[(most_inner_parth + places_after_most_inner +1) as usize..(input.len()) as usize];
-        input = ("").parse().unwrap();
-        input.push_str(inputcash1);
-        input.push_str(&inputcash2);
-        input.push_str(inputcash3);
-        num_of_parth = num_of_parth -1;
-        operator_places.clear();
-    }
-    //returns the answer as f32
-    return answer;
-}
-//determines if the characters in a string are numeric. written by me
-fn is_string_numeric(str: String) -> bool {
-    let mut numeric = 0;
-    for c in str.chars() {
-        if !c.is_numeric() {
-            numeric = 1;
-        }
-    }
-    if numeric == 1{
-        return false;
-    }else{
-        return true;
     }
 }
